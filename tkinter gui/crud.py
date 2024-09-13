97330bcf7aa0d7f32bb757435062f71ee30dd4f5
danukarangith <danukarangith456@gmail.com>
2024-09-13 23:58:25 +0530

    add user implement

--- tkinter gui/crud.py
+++ tkinter gui/crud.py
@@ -11,5 +11,19 @@ def connect_db():
         database="danukatest"   
     )
 
- 
+ def add_user():
+    name = entry_name.get()
+    email = entry_email.get()
+    if name and email:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users (name, email) VALUES (%s, %s)", (name, email))
+        conn.commit()
+        conn.close()
+        messagebox.showinfo("Success", "User added successfully")
+        entry_name.delete(0, END)
+        entry_email.delete(0, END)
+        display_users()
+    else:
+        messagebox.showwarning("Input error", "Please fill in all fields")
 
